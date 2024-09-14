f6e1c36dbe371ac96bc3868913db198953cad27e
Mehmet Yiter <76606674+mehmetyiter@users.noreply.github.com>
2024-09-14 05:55:48 -0400

    project structure
    This is the first basic structure of the project, we can build our project on this

---
+++ src/main/java/com/example/flashcard_app/FlashcardAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.flashcard_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardAppApplication.class, args);
+	}
+
+}
