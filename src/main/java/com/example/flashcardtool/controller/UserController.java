03b1d3f0c182239fe1e3ba1c41d324db3061043c
Joey Champ <2457464@johnabbottcollege.net>
2024-09-21 20:16:44 -0400

    fix admin dashboard route

--- src/main/java/com/example/flashcardtool/controller/UserController.java
+++ src/main/java/com/example/flashcardtool/controller/UserController.java
@@ -91,7 +91,7 @@ public class UserController {
             String role = authority.getAuthority();
 
             if (role.equals("ROLE_ADMIN")) {
-                return "redirect:/admin-dashboard";  // Admin dashboard'a yönlendir
+                return "redirect:/admin/admin-dashboard";  // Admin dashboard'a yönlendir
             } else if (role.equals("ROLE_TEACHER")) {
                 return "redirect:/teacher/dashboard";  // Öğretmen deck management sayfasına yönlendir
             } else if (role.equals("ROLE_STUDENT")) {
