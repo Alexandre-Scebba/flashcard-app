5fd3b917708d5f22f4305590a19fc7ed48a37c1f
Mehmet Yiter <76606674+mehmetyiter@users.noreply.github.com>
2024-09-23 13:55:21 -0400

    S3 service updated

--- src/main/java/com/example/flashcardtool/service/S3Service.java
+++ src/main/java/com/example/flashcardtool/service/S3Service.java
@@ -1,10 +1,12 @@
 package com.example.flashcardtool.service;
 
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -34,12 +36,18 @@ public class S3Service {
 
     public String uploadFile(MultipartFile file) throws IOException {
         String key = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        String region = System.getenv("AWS_REGION");  // Fetch the region from environment variables
+        String url = String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, key);
+
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
                 .build();
 
-        s3Client.putObject(putObjectRequest, Paths.get(file.getOriginalFilename()));
-        return "https://" + bucketName + ".s3.amazonaws.com/" + key;
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, key);
     }
+
+
 }
